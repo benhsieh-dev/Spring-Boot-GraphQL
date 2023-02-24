d188ea8179a436aefc7588dc6806a202d4097db6
Ben Hsieh <bqh5026@gmail.com>
2023-02-24 17:36:00 -0600

    Update files

--- src/main/java/dev/danvega/books/controller/BookController.java
+++ src/main/java/dev/danvega/books/controller/BookController.java
@@ -2,24 +2,26 @@ package dev.danvega.books.controller;
 
 import dev.danvega.books.model.Book;
 import dev.danvega.books.repository.BookRepository;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-//import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
 public class BookController {
-
     private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-//    @SchemaMapping(typeName = "Query",value = "allBooks")
-    @QueryMapping
+    @SchemaMapping(typeName = "Query",value = "allBooks")
     public List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    public Book findOne(@Argument Integer id) {
+        return bookRepository.findOne(id);
     }
 }
